76f8831f3a7270659fbb8ad42c6f5b77120f76f5
florMignini <flormignini29@gmail.com>
2024-05-10 08:42:20 -0300

    routing first approach

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NavmenuComponent } from "../shared/navmenu/navmenu.component";
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './dashboard.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-dashboard',
+    standalone: true,
+    templateUrl: './dashboard.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [RouterModule, NavmenuComponent]
 })
-export class DashboardComponent {}
+export default class DashboardComponent {}
