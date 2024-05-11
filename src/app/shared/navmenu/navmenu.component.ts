e0f812b2b053c75d4f170bd6b5d53db1cc44cf20
florMignini <flormignini29@gmail.com>
2024-05-11 08:58:29 -0300

    style update

--- src/app/shared/navmenu/navmenu.component.ts
+++ src/app/shared/navmenu/navmenu.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navmenu',
   standalone: true,
   imports: [
     CommonModule,
+    RouterModule
   ],
   templateUrl: './navmenu.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
