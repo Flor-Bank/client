c1b05d0dc71f9f48b0c836db4e76c5ffa2d793f4
florMignini <flormignini29@gmail.com>
2024-05-12 07:26:17 -0300

    update nested routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,23 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: "",
-    loadComponent: () => import("./dashboard/dashboard.component")
-  }
+    path: '',
+    loadComponent: () => import('./dashboard/dashboard.component'),
+    children: [
+      {
+        path: 'become-client',
+        title: 'Become Client',
+        loadComponent: () =>
+          import(
+            './dashboard/pages/user/become-client/become-client.component'
+          ),
+      },
+      {
+        path: 'login',
+        title: 'Login',
+        loadComponent: () =>
+          import('./dashboard/pages/user/login/login.component'),
+      },
+    ],
+  },
 ];
