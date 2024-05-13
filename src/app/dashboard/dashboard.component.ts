088b8fb5db97023de0322e874b2fff35f5ea5bd5
florMignini <flormignini29@gmail.com>
2024-05-13 09:14:54 -0300

    update main styles

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,6 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavmenuComponent } from "../shared/navmenu/navmenu.component";
-
 
 @Component({
     selector: 'app-dashboard',
