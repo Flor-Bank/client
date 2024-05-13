46ce1e5c546ea00d0684607737c6a8cd24defb0b
florMignini <flormignini29@gmail.com>
2024-05-13 09:15:12 -0300

    become client form

--- src/app/dashboard/pages/user/become-client/become-client.component.ts
+++ src/app/dashboard/pages/user/become-client/become-client.component.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'user-become-client',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './become-client.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class BecomeClientComponent {}
+export default class BecomeClientComponent {
+  constructor(private fb: FormBuilder) {}
+  public becomeClientForm: FormGroup = this.fb.group({});
+
+  onSubmit(): void {}
+}
