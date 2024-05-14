1c72e4ca2f3fbf295151a05603a024ebe0d6826f
florMignini <flormignini29@gmail.com>
2024-05-14 09:42:47 -0300

    update package & add icon

---
+++ src/app/shared/icons/checkIcon.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-icon-check',
+  standalone:true,
+  templateUrl: './checkIcon.component.html'
+})
+
+export default class CheckIconComponent {
+
+}
